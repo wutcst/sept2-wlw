eb78048b781be537a79ac6dad961d8ab539fe2e7
qianyuli <2643292850@qq.com>
2023-06-15 17:45:03 +0800

    提交接口文件

---
+++ src/Acquaintance/IData.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Acquaintance;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Rasmus Jensen
+ */
+public interface IData {
+
+    public boolean saveHighscore(ArrayList list);
+    public ArrayList loadHighscore();
+    /**
+     *
+     * @param key
+     * @return
+     */
+    public String getDescriptionText(String key);
+    public boolean saveGame(String savestring);
+    public String getLoadGame();
+
+}
