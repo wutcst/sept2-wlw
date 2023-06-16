259445f43858ecb5bf54bbad695963a5b8810f33
alwayssummerdays <2844157841@qq.com>
2023-06-16 21:05:40 +0800

    对代码结构进行了重构，重构时间匆忙没有填写注释 ，后面更新上去

---
+++ Gruppe 6 Semester Projekt/src/controller/SaveGame.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * 保存游戏的实现类
+ */
+public class SaveGame {
+    String loadfile;
+    
+    public SaveGame() {
+    }
+    //A method, which saves the saveString, into a txt file
+  public boolean saveGame(String saveString){
+      //A printwriter called save being initialized
+      PrintWriter save = null;
+      boolean bool = false;
+        try {
+            //New save printwriter, being initialized, which reads from and to the file SaveFile.txt
+            save = new PrintWriter("Gruppe 6 Semester Projekt/files/SaveFile.txt");
+            //The save then prints the content from the string, into the file
+            save.print(saveString);
+            //Sets the boolean bool to true
+            bool = true;
+            } catch (FileNotFoundException ex) {
+            Logger.getLogger(SaveGame.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            save.close();
+        }
+        return bool;
+  }
+                  //The method which, reads the content, from the SaveFile.txt, into a string, and returns it
+public String loadString(){
+        try {
+            
+            String filePath = "Gruppe 6 Semester Projekt/files/SaveFile.txt";
+            //The content from the file, gets read, and encoded
+            byte[] encoded = Files.readAllBytes(Paths.get(filePath));
+            //Then the contet, from the encoded byte, gets set into a string.
+            loadfile = new String(encoded, "utf-8");
+
+        } catch (IOException ex) {
+            Logger.getLogger(SaveGame.class.getName()).log(Level.SEVERE, null, ex);
+        }
+                    return loadfile;
+}
+    
+          
+    
+    
+}
